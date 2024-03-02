900a493415c4665020a0d319463c0de22358a5a4
Guilherme Froes Camba de Freitas <guilhermedevgames@gmail.com>
2024-03-01 21:12:28 -0300

    Init

---
+++ Packages/com.in-house-games.authentication/Runtime/Clients/MockServerAuth.cs
@@ -0,0 +1,27 @@
+﻿using Cdm.Authentication.OAuth2;
+
+namespace Cdm.Authentication.Clients
+{
+    public class MockServerAuth : MockServerAuth<AccessTokenResponse>
+    {
+        public MockServerAuth(Configuration configuration, string serverUrl) : base(configuration, serverUrl)
+        {
+        }
+    }
+
+    public class MockServerAuth<TAccessTokenResponse> : AuthorizationCodeFlow<TAccessTokenResponse> where TAccessTokenResponse : AccessTokenResponse
+    {
+        public const string AuthorizationPath = "/oauth/authorize";
+        public const string TokenPath = "/oauth/token";
+        
+        public override string authorizationUrl => $"{serverUrl}{AuthorizationPath}";
+        public override string accessTokenUrl => $"{serverUrl}{TokenPath}";
+        
+        public string serverUrl { get; }
+        
+        public MockServerAuth(Configuration configuration, string serverUrl) : base(configuration)
+        {
+            this.serverUrl = serverUrl;
+        }
+    }
+}
